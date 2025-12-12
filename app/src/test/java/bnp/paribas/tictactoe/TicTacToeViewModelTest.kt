94bd459651d812ede2893adc54275d3b120f7ca4
Hidden DEV <friddycrispyselection@hotmail.com>
2025-12-12 21:08:32 +0100

    Add additional testing for state of the board (red)

--- app/src/test/java/bnp/paribas/tictactoe/TicTacToeViewModelTest.kt
+++ app/src/test/java/bnp/paribas/tictactoe/TicTacToeViewModelTest.kt
@@ -43,4 +43,119 @@ class TicTacToeViewModelTest{
         assertEquals(Player.X, viewModel.currentPlayer)
         assertEquals(viewModel.board[0][1], Player.O)
     }
+
+    @Test
+    fun `A player is not able to select an already taken square`() {
+        viewModel.onBoardClick(0,0)
+        viewModel.onBoardClick(0,0)
+
+        assertEquals(Player.X, viewModel.board[0][0])
+        assertEquals(Player.O, viewModel.currentPlayer)
+    }
+
+    @Test
+    fun `When game is restarted during game then values are reset`() {
+        viewModel.onBoardClick(1,1)
+        viewModel.onBoardClick(1,2)
+        viewModel.restart()
+
+        // affected ones are empty
+        assertEquals(viewModel.board[1][1], Player.EMPTY)
+        assertEquals(viewModel.board[1][2], Player.EMPTY)
+        // others as well (still)
+        assertEquals(viewModel.board[0][0], Player.EMPTY)
+        assertEquals(viewModel.board[2][2], Player.EMPTY)
+
+        assertEquals(Player.X, viewModel.currentPlayer)
+    }
+
+    @Test
+    fun `When board is full without a winning condition, then no winner is selected`() {
+        /**
+         * X X O
+         * O O X
+         * X X O
+         */
+        // center column
+        viewModel.onBoardClick(0,1)
+        viewModel.onBoardClick(1,1)
+        viewModel.onBoardClick(2,1)
+        // end column
+        viewModel.onBoardClick(0,2)
+        viewModel.onBoardClick(1,2)
+        viewModel.onBoardClick(2,2)
+        // start column
+        viewModel.onBoardClick(0,0)
+        viewModel.onBoardClick(1,0)
+        viewModel.onBoardClick(2,0)
+
+        assertEquals(Player.EMPTY, viewModel.winner)
+    }
+
+
+    @Test
+    fun `When a player wins by row, then a winner is selected`() {
+        /**
+         * X X X
+         * O O .
+         * . . .
+         */
+        viewModel.onBoardClick(0, 0)
+        viewModel.onBoardClick(1, 0)
+        viewModel.onBoardClick(0, 1)
+        viewModel.onBoardClick(1, 1)
+        viewModel.onBoardClick(0, 2)
+
+        assertEquals(Player.X, viewModel.winner)
+    }
+
+    @Test
+    fun `When a player wins by column, then a winner is selected`() {
+        /**
+         * X O .
+         * X O .
+         * X . .
+         */
+        viewModel.onBoardClick(0, 0)
+        viewModel.onBoardClick(0, 1)
+        viewModel.onBoardClick(1, 0)
+        viewModel.onBoardClick(1, 1)
+        viewModel.onBoardClick(2, 0)
+
+        assertEquals(Player.X, viewModel.winner)
+    }
+
+    @Test
+    fun `When a player wins by diagonal (downward), then a winner is selected`() {
+        /**
+         * O X X
+         * . O X
+         * . . O
+         */
+        viewModel.onBoardClick(0, 1)
+        viewModel.onBoardClick(0, 0)
+        viewModel.onBoardClick(0, 2)
+        viewModel.onBoardClick(1, 1)
+        viewModel.onBoardClick(1, 2)
+        viewModel.onBoardClick(2, 2)
+
+        assertEquals(Player.O, viewModel.winner)
+    }
+
+    @Test
+    fun `When a player wins by diagonal (upward), then a winner is selected`() {
+        /**
+         * X X O
+         * . O X
+         * O . .
+         */
+        viewModel.onBoardClick(0, 0)
+        viewModel.onBoardClick(1, 1)
+        viewModel.onBoardClick(0, 1)
+        viewModel.onBoardClick(0, 2)
+        viewModel.onBoardClick(1, 2)
+        viewModel.onBoardClick(2, 0)
+
+        assertEquals(Player.O, viewModel.winner)
+    }
 }
