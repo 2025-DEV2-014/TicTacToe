8ce2bb6cad0b55c245ff47c95873face7a3cbece
Hidden DEV <friddycrispyselection@hotmail.com>
2025-12-12 22:18:48 +0100

    Add dummy screen for UI with some basic text

--- app/src/main/java/bnp/paribas/tictactoe/MainActivity.kt
+++ app/src/main/java/bnp/paribas/tictactoe/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import bnp.paribas.tictactoe.ui.theme.TicTacToeTheme
+import bnp.paribas.tictactoe.view.GameScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             TicTacToeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    GameScreen(
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
---
+++ app/src/main/java/bnp/paribas/tictactoe/view/GameScreen.kt
@@ -0,0 +1,34 @@
+package bnp.paribas.tictactoe.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun GameScreen(
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Tic Tac Toe",
+            fontSize = 30.sp,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Center
+        )
+    }
+}
