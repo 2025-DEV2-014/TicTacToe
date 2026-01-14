246d227f7efbee7b3b9ea4b5ca0d8f5c94ae54de
HIDDEN DEV <249335962+2025-DEV2-014@users.noreply.github.com>
2026-01-14 23:53:31 +0100

    refactor viewmodel to no longer have delegates of state properties. Remove obsolete viewmodelscope.launch that doesn't contain any suspend logic (green)

--- app/src/main/java/bnp/paribas/tictactoe/viewmodel/TicTacToeViewModel.kt
+++ app/src/main/java/bnp/paribas/tictactoe/viewmodel/TicTacToeViewModel.kt
@@ -1,57 +1,41 @@
 package bnp.paribas.tictactoe.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import bnp.paribas.tictactoe.model.GameState
 import bnp.paribas.tictactoe.model.Player
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 
 class TicTacToeViewModel(private val gameEngine: GameEngine = GameEngine()): ViewModel() {
 
     private val _gameState = MutableStateFlow(GameState())
     val gameState = _gameState.asStateFlow()
 
-    val board: Array<Array<Player>>
-        get() = gameState.value.board
-
-    val winner: Player
-        get() = gameState.value.winner
-
-    val currentPlayer: Player
-        get() = gameState.value.currentPlayer
-
-    val moves: Int
-        get() = gameState.value.moves
-
     fun restart() {
         _gameState.value = GameState()
     }
 
     fun onBoardClick(row: Int, col: Int) {
-        // Click has no effect if position is already taken OR the game is already over
-        if (board[row][col] != Player.EMPTY || winner != Player.EMPTY) {
-            return
-        }
+        _gameState.update { state ->
+            // Click has no effect if position is already taken OR the game is already over
+            if (state.board[row][col] != Player.EMPTY || state.winner != Player.EMPTY) {
+                return@update state
+            }
 
-        viewModelScope.launch {
-            val newBoard = board.copyOf()
-            newBoard[row][col] = currentPlayer
+            val newBoard = state.board.copyOf()
+            newBoard[row][col] = state.currentPlayer
 
-            val winner = gameEngine.checkForWinner(row, col, newBoard, currentPlayer)
-            val newMoveCount = moves + 1
+            val winner = gameEngine.checkForWinner(row, col, newBoard, state.currentPlayer)
+            val newMoveCount = state.moves + 1
             val newCurrentPlayer = gameEngine.getPlayerForMove(newMoveCount)
 
-            _gameState.update {
-                it.copy(
-                    board = newBoard,
-                    moves = newMoveCount,
-                    currentPlayer = newCurrentPlayer,
-                    winner = winner
-                )
-            }
+            state.copy(
+                board = newBoard,
+                moves = newMoveCount,
+                currentPlayer = newCurrentPlayer,
+                winner = winner
+            )
         }
     }
 }
